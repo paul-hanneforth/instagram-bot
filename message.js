77b79189ed473d5056f34a819d3480554fd845bc
Paul Hanneforth <paul.hanneforth.o@gmail.com>
2021-05-12 18:51:30 +0200

    initial commit

--- message.js
+++ message.js
@@ -1,18 +1,26 @@
 const errorCode = {
-    "1": "Wrong password!",
-    "2": "This post couldn't be found!"
-}
+    1: "Your password was incorrect!",
+    2: "Please wait a few minutes before trying to login!",
+    3: "Failed to convert user-readable number into computer-readable number!",
+    4: "Please login first!",
+    5: "Failed to goto identifier!",
+    6: "The search field couldn't be found! It seems like you aren't authenticated!",
+    7: "The input field for your credentials couldn't be found!"
+};
 const errorTemplate = (code) => {
     return {
-        "error": {
-            code,
-            message: errorCode[code]
-        }
-    }
-}
+        code,
+        message: errorCode[code]
+    };
+};
 const errorMessage = {
-    "wrongPassword": errorTemplate(1),
-    "postNotFound": errorTemplate(2)
-}
+    "incorrectPassword": errorTemplate(1),
+    "waitBeforeLogin": errorTemplate(2),
+    "failedToConvertNumber": errorTemplate(3),
+    "notAuthenticated": errorTemplate(4),
+    "failedToGotoIdentifier": errorTemplate(5),
+    "searchFieldNotFound": errorTemplate(6),
+    "inputFieldNotFound": errorTemplate(7)
+};
 
-module.exports = { errorMessage }
+module.exports = { errorMessage };
